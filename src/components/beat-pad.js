7f5eac6d66da9ffc2172664f4b3ff02bd209c3e8
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2021-03-27 17:42:12 -0700

    got rid of half of the sounds because not needed

--- src/components/beat-pad.js
+++ src/components/beat-pad.js
@@ -5,17 +5,9 @@ import Pad from "./pad";
 import padInfo from "./../pad-info";
 
 import akick1 from "./../sounds/kick1.wav";
-import akick2 from "./../sounds/kick2.wav";
-import akick3 from "./../sounds/kick3.wav";
 import aclap from "./../sounds/clap.wav";
 import ahihat from "./../sounds/hihat.wav";
 import ahihat2 from "./../sounds/hihat2.wav";
-import anoise from "./../sounds/noise.wav";
-import astab1 from "./../sounds/stab.wav";
-import astab2 from "./../sounds/stab2.wav";
-import astab3 from "./../sounds/stab3.wav";
-import asfx from "./../sounds/sfx.wav";
-import aglitch from "./../sounds/glitch.wav";
 import aloop1 from "./../sounds/loop1.wav";
 import aloop2 from "./../sounds/loop2.wav";
 import aloop3 from "./../sounds/loop3.wav";
@@ -36,62 +28,30 @@ function theSound(theKey) {
       kick1.play();
       break;
     case "c":
-      var kick2 = new Audio(akick2);
-      kick2.play();
-      break;
-    case "v":
-      var kick3 = new Audio(akick3);
-      kick3.play();
-      break;
-    case "b":
       var clap = new Audio(aclap);
       clap.play();
       break;
+    case "v":
+      var hh1 = new Audio(ahihat);
+      hh1.play();
+      break;
+    case "b":
+      var hh2 = new Audio(ahihat2);
+      hh2.play();
+      break;
     case "d":
-      var hihat = new Audio(ahihat);
-      hihat.play();
-      break;
-    case "f":
-      var hihat2 = new Audio(ahihat2);
-      hihat2.play();
-      break;
-    case "g":
-      var noise = new Audio(anoise);
-      noise.play();
-      break;
-    case "h":
-      var stab1 = new Audio(astab1);
-      stab1.play();
-      break;
-    case "r":
-      var stab2 = new Audio(astab2);
-      stab2.play();
-      break;
-    case "t":
-      var stab3 = new Audio(astab3);
-      stab3.play();
-      break;
-    case "y":
-      var sfx = new Audio(asfx);
-      sfx.play();
-      break;
-    case "u":
-      var glitch = new Audio(aglitch);
-      glitch.play();
-      break;
-    case "5":
       var loop1 = new Audio(aloop1);
       loop1.play();
       break;
-    case "6":
+    case "f":
       var loop2 = new Audio(aloop2);
       loop2.play();
       break;
-    case "7":
+    case "g":
       var loop3 = new Audio(aloop3);
      loop3.play();
       break;
-    case "8":
+    case "h":
       var loop4 = new Audio(aloop4);
       loop4.play();
       break;
--- src/pad-info.js
+++ src/pad-info.js
@@ -1,96 +1,48 @@
 const padInfo = [
     {
         id: 1,
-        buttonKey: "5",
-        color: "#8DFFF0",
-        colorTrigger: "#"
-    },
-    {
-        id: 2,
-        buttonKey: "6",
-        color: "#85FFC2",
-        colorTrigger: "#"
-    },
-    {
-        id: 3,
-        buttonKey: "7",
-        color: "#EEFFAA",
-        colorTrigger: "#"
-    },
-    {
-        id: 4,
-        buttonKey: "8",
-        color: "#84A9FF",
-        colorTrigger: "#"
-    },
-    {
-        id: 5,
-        buttonKey: "R",
-        color: "#C1FF82",
-        colorTrigger: "#"
-    },
-    {
-        id: 6,
-        buttonKey: "T",
-        color: "#FFB97B",
-        colorTrigger: "#"
-    },
-    {
-        id: 7,
-        buttonKey: "Y",
-        color: "#DA5EFF",
-        colorTrigger: "#"
-    },
-    {
-        id: 8,
-        buttonKey: "U",
-        color: "#FFD685",
-        colorTrigger: "#"
-    },
-    {
-        id: 9,
         buttonKey: "D",
         color: "#FFD685",
         colorTrigger: "#"
     },
     {
-        id: 10,
+        id: 2,
         buttonKey: "F",
         color: "#F781FF",
         colorTrigger: "#"
     },
     {
-        id: 11,
+        id: 3,
         buttonKey: "G",
         color: "#C2FF94",
         colorTrigger: "#"
     },
     {
-        id: 12,
+        id: 4,
         buttonKey: "H",
         color: "#FF8282",
         colorTrigger: "#"
     },
     {
-        id: 13,
+        id: 5,
         buttonKey: "X",
         color: "#FF8181",
         colorTrigger: "#"
     },
     {
-        id: 14,
+        id: 6,
         buttonKey: "C",
         color: "#938FFF",
         colorTrigger: "#"
     },
     {
-        id: 15,
+        id: 7,
         buttonKey: "V",
         color: "#FFFC9B",
         colorTrigger: "#"
     },
     {
-        id: 16,
+        id: 8,
         buttonKey: "B",
         color: "#7EBAFF",
         colorTrigger: "#"
